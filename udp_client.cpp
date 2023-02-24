163d55cd2e96898df22d947870264287c8f5ac48
Anton20241 <90354709+Anton20241@users.noreply.github.com>
2023-02-24 17:29:18 +0300

    Add files via upload

--- udp_client.cpp
+++ udp_client.cpp
@@ -1,9 +1,11 @@
 #include <cstdlib>
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/thread.hpp>
+#include <boost/chrono.hpp>
 #include <stdio.h>
 #include <string.h>
-
+#include "umba_crc_table.h"
 using boost::asio::ip::udp;
 using boost::asio::ip::address;
 
@@ -15,18 +17,53 @@ class UDPClient
 public:
     UDPClient(boost::asio::io_context& io_context)
     : io_context_(io_context), socket_(io_context){
+        std::cout << "UDP CLIENT IS RUNNING\n";
         sender_endpoint_ = udp::endpoint(address::from_string(IP), PORT);
         socket_.open(udp::v4());
     }
 
     void sendMsg() {
-        std::string msgToSend = "";
-        do{
-            std::getline(std::cin, msgToSend);
-            boost::system::error_code err;
-            auto sent = socket_.send_to(boost::asio::buffer(msgToSend), sender_endpoint_, 0, err);
-            std::cout << "Sent Payload --- " << sent << "\n";
-        } while (msgToSend != "q" || msgToSend != "Q");
+        auto start = std::chrono::high_resolution_clock::now();
+        uint8_t msg[] = {0xAA, 0xBB, 41,          //header
+                               0x1, 0x2, 0x3, 0x4, 0x5, //data
+                               0x1, 0x2, 0x3, 0x4, 0x5, //data
+                               0x1, 0x2, 0x3, 0x4, 0x5, //data
+                               0x1, 0x2, 0x3, 0x4, 0x5, //data 
+                               0x1, 0x2, 0x3, 0x4, 0x5, //data
+                               0x1, 0x2, 0x3, 0x4, 0x5, //data
+                               0x1, 0x1, 0x1,           //data_!fingers
+                               0x1, 0x1, 0x1, 0x1 //keepalive
+                               };
+        uint8_t crc8 = umba_crc8_table(msg, sizeof(msg));
+        uint8_t msgToSend[] = {0xAA, 0xBB, 41,          //header
+                               0x1, 0x2, 0x3, 0x4, 0x5, //data
+                               0x1, 0x2, 0x3, 0x4, 0x5, //data
+                               0x1, 0x2, 0x3, 0x4, 0x5, //data
+                               0x1, 0x2, 0x3, 0x4, 0x5, //data 
+                               0x1, 0x2, 0x3, 0x4, 0x5, //data
+                               0x1, 0x2, 0x3, 0x4, 0x5, //data
+                               0x1, 0x1, 0x1,           //data_!fingers
+                               0x1, 0x1, 0x1, 0x1, crc8 //keepalive
+                               };
+        static uint32_t send_count = 0;
+        boost::system::error_code err;
+        auto sent = socket_.send_to(boost::asio::buffer(msgToSend), sender_endpoint_, 0, err);
+        if (!err && sent > 0){
+            std::cout << "SEND TO UDP: ";
+            for (int i = 0; i < sizeof(msgToSend); i++){
+                printf("[%u]", msgToSend[i]);
+            }
+            std::cout << std::endl;
+            send_count++;
+            std::cout << "Sent Payload = " << sent << "\n";
+            std::cout << "send_count = " << send_count << "\n";
+            printf("[crc8 = %u\n]", crc8);
+            std::this_thread::sleep_for(std::chrono::microseconds(10000));
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float> duration = end - start;
+        float mls = duration.count() * 1000;
+        std::cout << "mls: " << mls << std::endl;
     }
 
     ~UDPClient() {
@@ -41,13 +78,20 @@ private:
 
 int main(int argc, char* argv[])
 {
+    // uint8_t msg1[] = {17, 9, 3, 1, 2, 3, 4, 5};
+    // uint8_t crc8_ = umba_crc8_table(msg1, sizeof(msg1));
+    // std::cout << "crc8_ = ";
+    // printf("%u\n", crc8_);
+    // while(1){;}
+
     try{
         boost::asio::io_context io_context;
         UDPClient udpClient(io_context);
-        udpClient.sendMsg();
+        while(1){
+            udpClient.sendMsg();
+        }
     } catch (std::exception e){
 		std::cerr << "Exeption: " << e.what() << std::endl;
     }
     return 0;
 }
- 
